f658f4fa75b1d3fa6d6016e231fe79e1c804002c
Gabriel Freitas <gabriel11.oliveira@alunos.ifsuldeminas.edu.br>
2026-02-22 10:14:09 -0300

    adicinar principal

--- src/main/java/br/edu/tds/projetopoo/Cliente.java
+++ src/main/java/br/edu/tds/projetopoo/Cliente.java
@@ -9,5 +9,29 @@ package br.edu.tds.projetopoo;
  * @author aluno
  */
 public class Cliente {
+    //Definição de atributos da classe
+    private String nome;
+    private int idade;
+    private String cpf;
     
+    //Definir o(s) meu(s) construtores
+    public Cliente(){
+        
+    }
+    
+    public Cliente(String n, int i, String cpf){
+        this.nome = n;
+        this.idade = i;
+        this.cpf = cpf;
+    }
+    
+    //Métodos get & set
+    
+    //Métodos funcionais
+    public void imprimeDadosCliente(){
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Idade: " + this.idade);
+        System.out.println("CPF: " + this.cpf);
+              
+    }
 }
---
+++ src/main/java/br/edu/tds/projetopoo/Principal.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.tds.projetopoo;
+
+/**
+ *
+ * @author aluno
+ */
+public class Principal {
+    
+    public static void main(String[] args){
+        System.out.println("Testando objetos da classe clientes");
+        //Usando a classe cliente
+        Cliente c1 = new Cliente("Gabriel",17,"241020008");
+        c1.imprimeDadosCliente();
+    }
+    
+}
