3faf9eb053a5df0ecb3d4c7df5c1d0c2a00275e3
Gabriel Freitas <gabriel11.oliveira@alunos.ifsuldeminas.edu.br>
2026-02-22 09:08:34 -0300

    Classe cliente adicionada

--- src/main/java/br/edu/tds/projetopoo/Cliente.java
+++ src/main/java/br/edu/tds/projetopoo/Cliente.java
@@ -1,17 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package br.edu.tds.projetopoo;
 
 /**
  *
  * @author aluno
  */
-public class ProjetoPOO {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Boa meu motoboy");
-    }
+public class Cliente {
+    
 }
